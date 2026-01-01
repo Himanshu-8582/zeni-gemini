8322e3bf4a9c8cd1c307e062c1c2e32366056360
Himanshu <himanshugupta8582@gmail.com>
2026-01-01 16:18:57 +0530

    everything is ok now

--- Frontend/src/env.js
+++ Frontend/src/env.js
@@ -1,5 +1,5 @@
 let isProd = true;   // false for localHost
 
-const server = isProd ? "https://zeni-gemini.vercel.app/" : "http://localhost:5000";
+const server = isProd ? "https://zeni-gemini.vercel.app" : "http://localhost:5000";
 
 export default server;
